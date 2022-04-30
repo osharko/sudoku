54f08819ed9c08d683693c83d50deb1878b69997
Luigi Minopoli <l.minopoli@be-tse.com>
2022-04-30 23:18:08 +0200

    Refactoring for color

--- internal/sudoku.go
+++ internal/sudoku.go
@@ -8,18 +8,63 @@ import (
 	"github.com/osharko/sudoku/internal/pogo"
 )
 
+var (
+	baseColor      = "\033[32m"
+	highlightColor = "\033[33m"
+)
+
+type sudokuSize uint8
+
+func (*sudokuSize) ToArray(arr []uint8) []sudokuSize {
+	ret := make([]sudokuSize, len(arr))
+	for i, v := range arr {
+		ret[i] = sudokuSize(v)
+	}
+	return ret
+}
+
+func (*sudokuSize) ToMatrix(matr [][]uint8) [][]sudokuSize {
+	ret := make([][]sudokuSize, len(matr))
+	for i, arr := range matr {
+		ret[i] = make([]sudokuSize, len(arr))
+		for j, v := range arr {
+			ret[i][j] = sudokuSize(v)
+		}
+	}
+	return ret
+}
+
+type sudokuElement struct {
+	value        sudokuSize
+	isStartValue bool
+}
+
+func (e *sudokuElement) FromSudokusize(matr [][]sudokuSize) [][]sudokuElement {
+	ret := make([][]sudokuElement, len(matr))
+	for i, arr := range matr {
+		ret[i] = make([]sudokuElement, len(arr))
+		for j, v := range arr {
+			ret[i][j] = sudokuElement{
+				value:        v,
+				isStartValue: v != 0,
+			}
+		}
+	}
+	return ret
+}
+
 type sudoku struct {
 	//Stats
-	currentCol        uint8 //Holds the current column
-	currentRow        uint8 //Holds the current row
-	iteration         uint8 //How many iteration have been done
-	startMissingValue uint8 //Holds the number of missing values at the beginning of the sudoku
+	currentCol        sudokuSize //Holds the current column
+	currentRow        sudokuSize //Holds the current row
+	iteration         sudokuSize //How many iteration have been done
+	startMissingValue sudokuSize //Holds the number of missing values at the beginning of the sudoku
 	//Data
-	grid [][]uint8 // Slice which hold all sudoku table.
+	grid [][]sudokuElement // Slice which hold all sudoku table.
 	//Configuration
-	size            uint8     // Size of a side of the grid. Ex: 9 for a 9x9 grid.
-	shapes          [][]uint8 // Represent all the shape of the grid. Each shape is made by the element's position into the grid.
-	requiredNumbers []uint8   //All the value that must be present in shape/column/row.
+	size            sudokuSize     // Size of a side of the grid. Ex: 9 for a 9x9 grid.
+	shapes          [][]sudokuSize // Represent all the shape of the grid. Each shape is made by the element's position into the grid.
+	requiredNumbers []sudokuSize   //All the value that must be present in shape/column/row.
 }
 
 // Since sudoku is a private struct, the only way to create a new sudoku is to use the SudokuFactory function.
@@ -27,14 +72,17 @@ type sudoku struct {
 func SudokuFactory(grid [][]uint8) (s sudoku) {
 	configuration := config.GetSudokuConfig()
 
+	var typeApp sudokuSize
+	var elApp sudokuElement
+
 	s = sudoku{
-		currentCol:      0,
-		currentRow:      0,
-		iteration:       0,
-		grid:            grid,
-		size:            configuration.SquareSize,
-		shapes:          configuration.Shapes,
-		requiredNumbers: configuration.RequiredNumbers,
+		currentCol:      sudokuSize(0),
+		currentRow:      sudokuSize(0),
+		iteration:       sudokuSize(0),
+		grid:            elApp.FromSudokusize(typeApp.ToMatrix(grid)),
+		size:            sudokuSize(configuration.SquareSize),
+		shapes:          typeApp.ToMatrix(configuration.Shapes),
+		requiredNumbers: typeApp.ToArray(configuration.RequiredNumbers),
 	}
 
 	s.startMissingValue = s.countMissingValues()
@@ -52,39 +100,39 @@ func (s *sudoku) Solve() {
 }
 
 // Return all the element in the current row.
-func (s *sudoku) getRowElements() (ret []uint8) {
-	for i := uint8(0); i < s.size; i++ {
-		ret = append(ret, s.grid[s.currentRow][i])
+func (s *sudoku) getRowElements() (ret []sudokuSize) {
+	for i := sudokuSize(0); i < s.size; i++ {
+		ret = append(ret, s.grid[s.currentRow][i].value)
 	}
 
 	return
 }
 
 // Return all the element in the current column.
-func (s *sudoku) getColElements() (ret []uint8) {
-	for i := uint8(0); i < s.size; i++ {
-		ret = append(ret, s.grid[i][s.currentCol])
+func (s *sudoku) getColElements() (ret []sudokuSize) {
+	for i := sudokuSize(0); i < s.size; i++ {
+		ret = append(ret, s.grid[i][s.currentCol].value)
 	}
 
 	return
 }
 
 // Accoring to the current row and column, returns all the element of the current shape.
-func (s *sudoku) getShapeElements() []uint8 {
+func (s *sudoku) getShapeElements() []sudokuSize {
 	sizeRoot := math.Sqrt(float64(s.size))
 
-	row := uint8((math.Floor(float64(s.currentRow)/sizeRoot) * sizeRoot))
-	col := uint8(math.Floor(float64(s.currentCol) / sizeRoot))
+	row := sudokuSize((math.Floor(float64(s.currentRow)/sizeRoot) * sizeRoot))
+	col := sudokuSize(math.Floor(float64(s.currentCol) / sizeRoot))
 
 	return s.getShape(row + col)
 }
 
 // Returns all element in a given shape.
-func (s *sudoku) getShape(shapePos uint8) (ret []uint8) {
+func (s *sudoku) getShape(shapePos sudokuSize) (ret []sudokuSize) {
 	for _, element := range s.shapes[shapePos] {
 		row := (element) / s.size
 		col := (element) % s.size
-		ret = append(ret, s.grid[row][col])
+		ret = append(ret, s.grid[row][col].value)
 	}
 
 	return
@@ -93,9 +141,9 @@ func (s *sudoku) getShape(shapePos uint8) (ret []uint8) {
 // Find all the missing number in the current row, column and shape,
 // Thene check if those 3 has 1 common missing number,
 // If so, fill the current cell with that number.
-func (s *sudoku) findValue() uint8 {
-	removeFromArray := func(origin []uint8, comparison []uint8) []uint8 {
-		rem := make([]uint8, 0)
+func (s *sudoku) findValue() sudokuSize {
+	removeFromArray := func(origin []sudokuSize, comparison []sudokuSize) []sudokuSize {
+		rem := make([]sudokuSize, 0)
 
 		for _, v := range origin {
 			for _, value := range comparison {
@@ -105,12 +153,12 @@ func (s *sudoku) findValue() uint8 {
 			}
 		}
 
-		return pogo.FilterArray(origin, func(o uint8) bool {
+		return pogo.FilterArray(origin, func(o sudokuSize) bool {
 			return !pogo.ContainsArray(rem, o)
 		})
 	}
 
-	values := make([]uint8, len(s.requiredNumbers))
+	values := make([]sudokuSize, len(s.requiredNumbers))
 	copy(values, s.requiredNumbers)
 	r, c, h := s.getRowElements(), s.getColElements(), s.getShapeElements()
 	values = removeFromArray(values, r)
@@ -126,23 +174,26 @@ func (s *sudoku) findValue() uint8 {
 
 // If there's no 0 value into the grid, then the sudoku is complete.
 func (s *sudoku) isComplete() bool {
-	return pogo.EveryInArray(s.grid, func(row []uint8) bool {
-		return !pogo.ContainsArray(row, 0)
+	return pogo.EveryInArray(s.grid, func(row []sudokuElement) bool {
+		flat := pogo.MapArray(row, func(e sudokuElement) sudokuSize {
+			return e.value
+		})
+		return !pogo.ContainsArray(flat, 0)
 	})
 }
 
 func (s *sudoku) findMissingValue() {
 	for i, row := range s.grid {
 		for j, value := range row {
-			if value != 0 {
+			if value.value != 0 {
 				continue
 			}
 
-			s.currentRow = uint8(i)
-			s.currentCol = uint8(j)
+			s.currentRow = sudokuSize(i)
+			s.currentCol = sudokuSize(j)
 
 			if v := s.findValue(); v != 0 {
-				s.grid[i][j] = v
+				s.grid[i][j].value = v
 				return
 			}
 		}
@@ -150,11 +201,11 @@ func (s *sudoku) findMissingValue() {
 }
 
 // Return the number of 0 into the grid.
-func (s *sudoku) countMissingValues() uint8 {
-	count := uint8(0)
+func (s *sudoku) countMissingValues() sudokuSize {
+	count := sudokuSize(0)
 	for _, row := range s.grid {
 		for _, value := range row {
-			if value == 0 {
+			if value.value == 0 {
 				count++
 			}
 		}
@@ -168,11 +219,17 @@ func (s *sudoku) PrintGrid() {
 	fmt.Printf("\n\t\t\t")
 
 	root := int(math.Sqrt(float64(s.size)))
+	var color string
 
 	for i, row := range s.grid {
 		for j, value := range row {
 			//Print the current value
-			fmt.Printf("%d ", value)
+			if value.isStartValue {
+				color = baseColor
+			} else {
+				color = highlightColor
+			}
+			fmt.Printf("%s%d ", color, value.value)
 			//Reaching the end of the shape, print a tab.
 			if (j+1)%root == 0 {
 				fmt.Printf("\t")
